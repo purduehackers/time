270284f8ad9e8d2f50282bf5022f41d96ba76ca8
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-09 10:40:32 -0400

    Add charges to `convertToLightning`

--- src/time.ts
+++ src/time.ts
@@ -3,32 +3,34 @@ import getParts from './utils/get-parts'
 import msToTime from './utils/ms-to-time'
 import validate from './utils/validate'
 
-export const convertToLightning = (time: Date): LightningString => {
-  const millisPerSpark = 21093.75 // 86400000 / 16^3
+export const convertToLightning = (time: Date) => {
+  const millisPerCharge = 1318.359375 // 86400000 / 16^4
 
   const millis =
     1000 * 60 * 60 * time.getHours() +
     1000 * 60 * time.getMinutes() +
     1000 * time.getSeconds() +
     time.getMilliseconds()
-  const totalSparks = millis / millisPerSpark
+  const totalCharges = millis / millisPerCharge
+  const totalSparks = totalCharges / 16
   const totalZaps = totalSparks / 16
   const totalBolts = totalZaps / 16
 
+  const charges = Math.floor(totalCharges) % 16
   const sparks = Math.floor(totalSparks) % 16
   const zaps = Math.floor(totalZaps) % 16
   const bolts = Math.floor(totalBolts) % 16
 
   const lightningString =
-    bolts.toString(16) + '~' + zaps.toString(16) + '~' + sparks.toString(16)
-  const colors = getColors(lightningString)
+    bolts.toString(16) +
+    '~' +
+    zaps.toString(16) +
+    '~' +
+    sparks.toString(16) +
+    (charges > 0 ? '|' + charges.toString(16) : '')
   return {
     lightningString,
-    colors,
-    originalTimeString: time.toLocaleTimeString([], {
-      hour: 'numeric',
-      minute: '2-digit'
-    })
+    originalTimeString: time.toLocaleTimeString()
   }
 }
 
