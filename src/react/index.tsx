3924754700e0613c9c6664d2e0d00d6f37b90f1b
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-23 04:33:17 -0400

    Add hook

---
+++ src/react/index.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import { format as formatTime } from 'date-fns'
+import { LightningTime, MILLIS_PER_CHARGE } from '../time'
+
+export function useLightningTimeClock() {
+  const [lightningTime, setLightningTime] = useState('')
+  const [normalTime, setNormalTime] = useState('')
+
+  useEffect(() => {
+    const update = () => {
+      const now = new Date()
+
+      const lt = new LightningTime()
+      const convertedTime = lt.convertToLightning(now).lightningString
+      setLightningTime(convertedTime)
+
+      const formattedTime = formatTime(now, 'h:mm a')
+      setNormalTime(formattedTime)
+    }
+
+    update()
+
+    const now = new Date()
+    const millis =
+      1000 * 60 * 60 * now.getHours() +
+      1000 * 60 * now.getMinutes() +
+      1000 * now.getSeconds() +
+      now.getMilliseconds()
+    const remainingMillis = MILLIS_PER_CHARGE - (millis % MILLIS_PER_CHARGE)
+
+    const timer = setTimeout(() => {
+      update()
+
+      const interval = setInterval(update, MILLIS_PER_CHARGE)
+
+      return () => clearInterval(interval)
+    }, remainingMillis)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return { lightningTime, normalTime }
+}
