9c134693a4cbf283447559544026c525a77b0f3c
Matthew Stanciu <mattbstanciu@gmail.com>
2023-12-10 22:43:37 -0500

    update file name

--- src/preact/index.tsx
+++ src/preact/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'preact/hooks'
-import { useLightningTimeClockCore } from '../use-lightning-time-clock-core'
+import { useLightningTimeClockCore } from '../use-lightning-time-clock'
 
 export function useLightningTimeClock() {
   return useLightningTimeClockCore({ useState, useEffect })
--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useLightningTimeClockCore } from '../use-lightning-time-clock-core'
+import { useLightningTimeClockCore } from '../use-lightning-time-clock'
 
 export function useLightningTimeClock() {
   return useLightningTimeClockCore({ useState, useEffect })
