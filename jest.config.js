d9533dade1c1256d47d6e930ceb5e7519d0276fc
Matthew Stanciu <mattbstanciu@gmail.com>
2023-12-11 11:17:35 -0500

    Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,11 @@ module.exports = {
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }],
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       './config/fileTransformer.js',
   },
   transformIgnorePatterns: ['./node_modules/(?!rgb-hex)'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
 }
