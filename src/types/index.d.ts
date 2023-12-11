42916fdea24a7e24b3c15bef05dc569c8d20d724
Matthew Stanciu <mattbstanciu@gmail.com>
2023-12-10 21:57:24 -0500

    Untested preact support

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -35,3 +35,11 @@ export interface StaticColors {
   zapColors: number[]
   sparkColors: number[]
 }
+
+export type GenericUseState = <T>(
+  initialState: T | (() => T)
+) => [T, (newState: T | ((prevState: T) => T)) => void]
+export type GenericUseEffect = (
+  effect: () => void | (() => void | undefined),
+  deps?: ReadonlyArray<any>
+) => void
