865aac3866097bc9807b214185f3e586c65777d4
Matthew Stanciu <mattbstanciu@gmail.com>
2023-12-13 13:20:29 -0500

    reorg a bit

--- test/react-clock/react-clock.test.tsx
+++ test/react-clock/react-clock.test.tsx
@@ -8,6 +8,19 @@ import React from 'react'
 import { LightningTime } from '../../src'
 import { format } from 'date-fns'
 
+function getDesiredValues() {
+  const now = new Date()
+  const lt = new LightningTime()
+  const { lightningString, colors } = lt.convertToLightning(now)
+  const formattedNowString = format(now, 'h:mm a')
+
+  return {
+    lightningString,
+    formattedNowString,
+    colors
+  }
+}
+
 describe('react clock', () => {
   it('renders a react clock', () => {
     render(<Clock />)
@@ -17,17 +30,22 @@ describe('react clock', () => {
     expect(screen.getByTestId('zap color')).toBeDefined()
     expect(screen.getByTestId('spark color')).toBeDefined()
   })
-  it('renders the correct lightning time', () => {
+  it('renders the correct lightning time string', () => {
     render(<Clock />)
-    const now = new Date()
-    const lt = new LightningTime()
-    const { lightningString, colors } = lt.convertToLightning(now)
-    const formattedNowString = format(now, 'h:mm a')
+    const { lightningString } = getDesiredValues()
 
-    // this will occasionally fail if the test is run right before a new spark
-    // TODO: find a less flaky way to test this
     expect(screen.getByTestId('lightning string').innerHTML).toEqual(lightningString)
+  })
+  it('renders the correct formatted normal time', () => {
+    render(<Clock />)
+    const { formattedNowString } = getDesiredValues()
+
     expect(screen.getByTestId('formatted normal time').innerHTML).toEqual(formattedNowString)
+  })
+  it('renders the correct colors', () => {
+    render(<Clock />)
+    const { colors } = getDesiredValues()
+
     expect(screen.getByTestId('bolt color').innerHTML).toEqual(colors.boltColor)
     expect(screen.getByTestId('zap color').innerHTML).toEqual(colors.zapColor)
     expect(screen.getByTestId('spark color').innerHTML).toEqual(colors.sparkColor)
